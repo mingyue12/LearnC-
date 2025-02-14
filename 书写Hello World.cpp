0356c2510841a3b497d0fc1424d3d381f4e04110
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2025-02-14 22:16:12 +0800

    modified:   "output/\344\271\246\345\206\231Hello World.exe" 	modified:   "\344\271\246\345\206\231Hello World.cpp"

--- 书写Hello World.cpp
+++ 书写Hello World.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	cout << "你好" << endl;//输出你好
-	cout << "Hello World!" << endl;//输出Hello World!
+	cout << "OK" << endl;
+	cout << "你好" << endl;			// 输出你好
+	cout << "Hello World!" << endl; // 输出Hello World!
 	return 0;
 }
