76d12184619d7fb7c33d877d265e7a6c4e9706db
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2025-02-15 15:26:58 +0800

    C++
    1

--- 2数据类型/03 浮点型.cpp
+++ 2数据类型/03 浮点型.cpp
@@ -6,12 +6,15 @@ int main()
 
     //1.单精度浮点型
     //2.双精度浮点型
+    //默认情况下，输出一个小数，会显示6位有效数字
     float f = 3.14f;
     double d = 3.14;
     cout << "f = " << f << endl;
     cout << "d = " << d << endl;
     
-    
+    cout << "size of float = " << sizeof(float) << endl;
+    cout << "size of double = " << sizeof(double) << endl;
+
     system("pause");
     return 0;
 }
